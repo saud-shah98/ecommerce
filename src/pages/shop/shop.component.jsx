1cb810fdfa5c2d6016a291f857695a2151b99585
saud-shah98 <shahm7@vcu.edu>
2020-02-24 00:55:33 -0500

    Directory State, Collection State Into Redux

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,12 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data.js';
+import CollectionsOverview from '../../components/collections-overview/collection-overviews.component'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    <CollectionsOverview />
+  </div>
+);
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className='shop-page'>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
 
 export default ShopPage;
