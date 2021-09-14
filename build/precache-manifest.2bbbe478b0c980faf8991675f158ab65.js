399424896d0140811e0ebcf0f9d9662ccc319ac9
MSS <mss@MSSs-MacBook-Pro.local>
2021-09-13 21:33:59 -0400

    build

---
+++ build/precache-manifest.2bbbe478b0c980faf8991675f158ab65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53eb74100127f2c2f19e2535387a9dcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0826876391d94b21fd23",
+    "url": "/static/css/main.2abe00ae.chunk.css"
+  },
+  {
+    "revision": "8f622cf7df002dca48fd",
+    "url": "/static/js/2.a40ce7a6.chunk.js"
+  },
+  {
+    "revision": "0826876391d94b21fd23",
+    "url": "/static/js/main.567d955c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
